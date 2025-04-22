641d1ad5f50ecfa82384305dca941d22807e7504
imannfazal <imannfazal@gmail.com>
2025-04-22 15:03:22 +0500

    print

--- cat.py
+++ cat.py
@@ -1,9 +1,12 @@
-print("While:")
+print("\nWhile:")
 i=3
 while i!=0:
     print("meow")
     i -=1
 
-print("For: ")
+print("\nFor:")
 for _ in range (10):
     print("meow")
+
+print("\nusing print")
+print("meow\n" *3, end="")
