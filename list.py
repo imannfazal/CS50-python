0227a97ed89e8cf035be0c24d1cd8078d031433e
imannfazal <imannfazal@gmail.com>
2025-04-22 17:57:47 +0500

    dict in list

--- list.py
+++ list.py
@@ -7,3 +7,12 @@ members = {"hermoine": "gryffindor", "draco": "slytherin"}
 
 for member in members:
     print(member, members[member], sep=" belongs to ")
+
+data = [
+    {"name":"hermoine", "house": "gryffindor", "patronus": "Otter"},
+    {"name":"harry", "house": "gryffindor", "patronus": "stag"},
+    {"name":"draco", "house": "slytherin", "patronus": None}
+]
+
+for datas in data:
+    print(datas["name"], datas["house"], datas["patronus"], sep=", ")
