7d940106c7af7f7b26cb553ee35590102a05f75d
imannfazal <imannfazal@gmail.com>
2025-04-24 17:31:23 +0500

    aaaaaaaaaaa

--- itunes.py
+++ itunes.py
@@ -1,8 +1,11 @@
 import sys
 import requests
+import json
 
 if len(sys.argv)<1:
     sys.exit()
-respose = requests.get("https://itunes.apple.com/search?term=jack+johnson&limit=25")
+respose = requests.get("https://itunes.apple.com/search?term=weezer&limit=10")
 
-print(respose.json())
+o = respose.json()
+for track in o["results"]:
+    print(track["trackCensoredName"])
