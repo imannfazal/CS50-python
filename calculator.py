3188158b61a214611efc3eb26e4f8404eff0aaaf
imannfazal <imannfazal@gmail.com>
2025-04-24 13:10:34 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,16 @@
-x= int(input("Enter x: "))
-y=int(input("Enter y: "))
+while True:
+    try:
+        x= int(input("Enter x: "))
+    except ValueError:
+        print("x is not integer")
+    else: 
+        break
+        
+while True:
+    try:
+        y=int(input("Enter y: "))
+    except ValueError:
+        print("y is not integer")
+    else:
+        break
 print(x+y)
