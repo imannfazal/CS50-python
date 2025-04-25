6138e956657840b23564f5cd43703080cdcb2904
imannfazal <imannfazal@gmail.com>
2025-04-25 16:58:14 +0500

    sorted

--- names.py
+++ names.py
@@ -1,8 +1,8 @@
 with open("Names.txt", "r") as file:
     names = file.readlines()
 
-for name in names:
-    if len(name)==7:
+for name in sorted(names):
+    if len(name)<=7:
         print(name, end="")
 
 
