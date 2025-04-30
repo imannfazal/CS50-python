5ba6a60464351677b5e4b2c7f8f1c7ac2887371b
imannfazal <imannfazal@gmail.com>
2025-04-30 17:49:01 +0500

    __init__ constructor

--- OOP/classes.py
+++ OOP/classes.py
@@ -1,5 +1,7 @@
 class Student:
-    ...
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
 
 def main():
     student = get_student()
@@ -7,10 +9,9 @@ def main():
 
 def get_student():
     # making object of Student class "student"
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
 
 if __name__ == "__main__":
     main()
