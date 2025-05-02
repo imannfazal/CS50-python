ee058fb09b36c24e22952a8e5d98ff9fe6d5933b
imannfazal <imannfazal@gmail.com>
2025-05-02 15:55:32 +0500

    getter and setter

--- OOP/classes.py
+++ OOP/classes.py
@@ -1,33 +1,45 @@
 class Student:
-    def __init__(self, name, house, patronus):
+    def __init__(self, name, house):
         if not name:
             raise ValueError("Name not entered!")
         self.name = name
         self.house = house
-        self.patronus = patronus
+        # self.patronus = patronus
     def __str__(self):
-        return(f"{self.name} from {self.house} does {self.patronus}")
-    def charm(self):
-        match self.patronus:
-            case "Stag":
-                return "🦌"
-            case "Otter":
-                return "🦦"
-            case "Jack Russell":
-                return "🐶"
-            case _:
-                return "🥢"
+        return(f"{self.name} from {self.house}")
+
+    # getter
+    @property
+    def house(self):
+        return self._house
+    
+    @house.setter
+    def house(self, house):
+        if house not in ("Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"):
+            raise ValueError("Invalid house!")
+        self._house = house
+
+    # def charm(self):
+    #     match self.patronus:
+    #         case "Stag":
+    #             return "🦌"
+    #         case "Otter":
+    #             return "🦦"
+    #         case "Jack Russell":
+    #             return "🐶"
+    #         case _:
+    #             return "🥢"
 
 def main():
     student = get_student()
-    print(student.charm())
+    print(student)
 
 def get_student():
     # making object of Student class "student"
     name = input("Name: ")
     house = input("House: ")
-    charm = input("Charm: ")
-    return Student(name, house, charm)
+    # charm = input("Charm: ")
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
